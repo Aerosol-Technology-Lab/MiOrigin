26c3fa49b58d2b5da8ba3854eeed9580bff6989b
Charlemagne Wong <frian13@hotmail.com>
2021-07-20 15:38:27 -0500

    Completed ts conversion

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -3,10 +3,8 @@ import './App.css';
 import { useState ,useEffect } from 'react';
 import './BLE_Handler';
 import { triggerNotification, waitForNotification } from './BLE_Handler';
+import { connectToDevice, disconnectToDevice } from './Device_BLE';
 
-const SERVICE_UUID = "9f9ece14-b513-4cdb-b0b8-9e5fa3b9fef2";
-
-const CHARACTERISTIC_UUID = "f5db5ef9-c1d0-4d1b-8907-d3f2075872c5";
 
 const BRANCHES = {
   main: "main",
@@ -14,6 +12,21 @@ const BRANCHES = {
 };
 
 const CONST_BINARY_PATH = 'https://github.com/cmasterx/MiOrigin/blob/{0}/firmware/binaries/firmware.bin?raw=true';
+
+declare global {
+  interface Window {
+    response: any;
+    device: any;
+    server: any;
+    service: any;
+    characteristic: any;
+    waitForNotification: any;
+    getPromise: any;
+    bleNotify: any;
+    fileResult: any;
+    test: any;
+  }
+}
 
 window.waitForNotification = waitForNotification;
 window.getPromise = waitForNotification;
@@ -29,54 +42,27 @@ function App() {
 
   const [number, setnumber] = useState(0)
   
-  const connectToDevice = async () => {
-    if (device === null) {
-      console.log("Requesting");
-      let pDevice = await navigator.bluetooth.requestDevice({filters: [{services: [SERVICE_UUID]}]});
-      let pServer = await pDevice.gatt.connect();
-      let pService = await pServer.getPrimaryService(SERVICE_UUID);
-      let pCharacteristic = await pService.getCharacteristic(CHARACTERISTIC_UUID);
-      // setDevice(await navigator.bluetooth.requestDevice({filters: [{services: [SERVICE_UUID]}]}));
-      // setserver(await device.gatt.connect());
-
-      setDevice(pDevice);
-      setServer(pServer);
-      setService(pService);
-      setCharacteristic(pCharacteristic);
-
-      console.log("Here I am");
-    }
-    else {
-      console.log("Device is initialized. Nothing to do.");
-    }
+  
     
     window.device = device;
     window.server = server;
     window.service = service;
     window.characteristic = characteristic;
     console.log('Howdy!')
-  };
 
-  const disconnectToDevice = async() => {
-    if (device !== null) {
-      server.disconnect();
-      setDevice(null);
-      setServer(null);
-      setService(null);
-      setCharacteristic(null);
-    }
-  };
+
   
-  const sendMTURequest = () => {
-    if (device !== null) {
-      let buffer = new Uint8Array(6);
-      let props = new Uint16Array(1);
-      buffer[2] = 0x0D;
-      buffer[3] = 0x01;
+  // const sendMTURequest = () => {
+  //   if (device !== null) {
+  //     let buffer = new Uint8Array(6);
+  //     let props = new Uint16Array(1);
+  //     buffer[2] = 0x0D;
+  //     buffer[3] = 0x01;
 
-      window.response = characteristic.writeValueWithResponse(buffer);
-    }
-  };
+
+  //     window.response = characteristic.writeValueWithResponse(buffer);
+  //   }
+  // };
 
   const startCounter = async () => {
     var n = number;
@@ -104,23 +90,24 @@ function App() {
     }
   }, [])
   
-  const showFile = async (input) => {
-    console.log ("I am here!");
-    const reader = new FileReader();
+  const showFile = async (input: any) => {
+    window.test = input;
+    // console.log ("I am here!");
+    // const reader = new FileReader();
 
-    const filePromise = new Promise((resolve, reject) => {
-      reader.onload = (event) => {
-        resolve(event.target.result);
-      };
+    // const filePromise = new Promise((resolve, reject) => {
+    //   reader.onload = (event) => {
+    //     resolve(event.target.result);
+    //   };
 
-      reader.onerror = (err) => {
-        reject(err);
-      };
-    });
+    //   reader.onerror = (err) => {
+    //     reject(err);
+    //   };
+    // });
     //https://stackoverflow.com/questions/56338154/howto-convert-file-to-byte-stream-in-javascript
     // reader.readAsDataURL(input.target.files[0]);
-    reader.readAsArrayBuffer(input.target.files[0]);
-    window.fileResult = await filePromise;
+    // reader.readAsArrayBuffer(input.target.files[0]);
+    // window.fileResult = await filePromise;
     // let file = input.files[0];
   
     // alert(`File name: ${file.name}`); // e.g my.png
@@ -130,9 +117,9 @@ function App() {
   return (
     <div className="App">
       <p>{number}</p>
-      <button onClick={ connectToDevice } >Connect</button>
+      <button onClick={ () => connectToDevice() } >Connect</button>
       <button onClick={ disconnectToDevice }>Disconnect</button>
-      <button onClick= { sendMTURequest }>Send MTU Request</button>
+      {/* <button onClick= { sendMTURequest }>Send MTU Request</button> */}
       <button onClick= { triggerNotification }>Trigger</button>
       <input type="file" onChange={ showFile }></input>
       {/* <input type="file" /> */}
--- website/src/Device_BLE.ts
+++ website/src/Device_BLE.ts
@@ -1,9 +1,17 @@
 import { triggerNotification, waitForNotification } from './BLE_Handler';
+import { Packet_t, Props_t, Command_t } from './PacketDefinitions';
 
-var pDevice = null as BluetoothDevice;
-var pServer = null as BluetoothRemoteGATTServer;
-var pService = null as BluetoothRemoteGATTService;
-var pCharacteristic = null as BluetoothRemoteGATTCharacteristic;
+
+// BLE UUIDs
+const SERVICE_UUID = "9f9ece14-b513-4cdb-b0b8-9e5fa3b9fef2";
+const CHARACTERISTIC_UUID = "f5db5ef9-c1d0-4d1b-8907-d3f2075872c5";
+
+
+
+var pDevice = null as BluetoothDevice | null;
+var pServer: BluetoothRemoteGATTServer | null = null;
+var pService = null as BluetoothRemoteGATTService | null;
+var pCharacteristic = null as BluetoothRemoteGATTCharacteristic | null;
 
 var mtu = 20;       // useable MTU -> if actual MTU is 23, useable is 23 - 3 = 20
 
@@ -11,9 +19,8 @@ const connectToDevice = async (setMTU = 0) => {
     if (pDevice === null) {
       console.log("Requesting");
       pDevice = await navigator.bluetooth.requestDevice({filters: [{services: [SERVICE_UUID]}]});
-      pServer = await pDevice.gatt.connect();
-      pService = await pServer.getPrimaryService(SERVICE_UUID);
-      pCharacteristic = await pService.getCharacteristic(CHARACTERISTIC_UUID);
+      pService = await pServer?.getPrimaryService(SERVICE_UUID) || null;
+      pCharacteristic = await pService?.getCharacteristic(CHARACTERISTIC_UUID) || null;
       // setDevice(await navigator.bluetooth.requestDevice({filters: [{services: [SERVICE_UUID]}]}));
       // setserver(await device.gatt.connect());
 
@@ -30,14 +37,22 @@ const connectToDevice = async (setMTU = 0) => {
 
 const disconnectToDevice = async() => {
     if (pDevice !== null) {
-      pServer.disconnect();
+      pServer?.disconnect();
       pServer = null;
       pService = null;
       pCharacteristic = null;
     }
 };
 
+const getDefaultPacket = () => {
+  return [new Packet_t(mtu), new Props_t(1), new Command_t(1)];
+}
+
 const changeMTU = async (setMTU: Number) => {
+
+  var [sendPacket , props, command] = getDefaultPacket();
+  
+  
 
 }
 
@@ -49,4 +64,7 @@ const uploadFile = async(file: File | Blob) => {
     
 }
 
-
+export {
+  connectToDevice,
+  disconnectToDevice
+}
