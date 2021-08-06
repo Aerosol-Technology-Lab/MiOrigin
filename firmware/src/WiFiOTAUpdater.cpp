c6059e61d0f7c8de5d0d39fc1482719de0778312
Charlemagne Wong <frian13@hotmail.com>
2021-08-06 18:50:01 -0500

    Fix issues

--- firmware/src/WiFiOTAUpdater.cpp
+++ firmware/src/WiFiOTAUpdater.cpp
@@ -1,4 +1,5 @@
 #include "WiFiOTAUpdater.h"
+#include <Arduino.h>
 #include <stddef.h>
 #include <string.h>
 
@@ -16,8 +17,14 @@ const WiFiOTAUpdater::branch_t WiFiOTAUpdater::BRANCHES[2] = {
 
 int WiFiOTAUpdater::branchInList(const char *key)
 {
+    Serial.println("Here in this");
     for (int i = 0; i < sizeof(BRANCHES) / sizeof(BRANCHES[0]); ++i) {
-        if (!strcmp(BRANCHES[i].name, key)) return i;
+        Serial.println("Over here");
+        if (!strcmp(BRANCHES[i].name, key)) {
+            Serial.println("Found");
+            return i;
+        }
+        Serial.println("not found");
     }
 
     return -1;
